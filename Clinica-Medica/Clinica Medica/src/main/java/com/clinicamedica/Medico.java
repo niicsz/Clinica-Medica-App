112e1c33699117776bc5e81f925714d878afb0ee
Nicolas Bezerra Bini <78541669+niicsz@users.noreply.github.com>
2024-06-17 19:06:45 -0300

    Delete Clinica-Medica/Clinica Medica directory

--- Clinica-Medica/Clinica Medica/src/main/java/com/clinicamedica/Medico.java
+++
@@ -1,6 +0,0 @@
-
-public class Medico extends Usuario {
-    public Medico(String nome, String cpf, int idade, String email, String senha) {
-        super(nome, cpf, idade, email, senha);
-    }
-}
