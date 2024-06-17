bbce602069a90615efda7380c25319651eb8b65b
Nicolas Bezerra Bini <78541669+niicsz@users.noreply.github.com>
2024-06-17 11:02:45 -0300

    Update ConexãoBD

--- ConexaoBD.java
+++ ConexaoBD.java
@@ -4,8 +4,16 @@ import java.sql.SQLException;
 
 public class ConexaoBD {
     private static final String URL = "jdbc:mysql://localhost:3306/clinica_medica";
-    private static final String USUARIO = "root";
-    private static final String SENHA = "";
+    private static final String USUARIO = "root"; 
+    private static final String SENHA = "";        
+    static {
+        try {
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Erro ao carregar o driver JDBC", e);
+        }
+    }
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
