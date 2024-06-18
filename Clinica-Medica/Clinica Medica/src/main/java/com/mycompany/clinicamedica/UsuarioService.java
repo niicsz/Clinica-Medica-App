e801d7f94cc77e81e51b17e0965b8cad82cd353e
Nicolas Bezerra Bini <78541669+niicsz@users.noreply.github.com>
2024-06-18 16:41:28 -0300

    Delete Clinica-Medica/Clinica Medica directory

--- Clinica-Medica/Clinica Medica/src/main/java/com/mycompany/clinicamedica/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.clinicamedica;
-
-public class UsuarioService {
-    private UsuarioDAO usuarioDAO = new UsuarioDAO();
-
-    public void incluirUsuario(Usuario usuario) {
-        usuarioDAO.incluirUsuario(usuario);
-    }
-}
