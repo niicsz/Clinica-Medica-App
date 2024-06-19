7b02cbf80b63e1b86a9069723dbd8a94b45ba8f2
Nicolas Bezerra Bini <78541669+niicsz@users.noreply.github.com>
2024-06-19 11:41:51 -0300

    Update clinica_medica.sql

--- Clinica Medica/sql/clinica_medica.sql
+++ Clinica Medica/sql/clinica_medica.sql
@@ -1,2 +1,36 @@
+CREATE DATABASE clinicamedica;
 
+USE clinicamedica;
 
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    idade INT NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE medicos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    especialidade VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE pacientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    idade INT NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE consultas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    paciente_id INT,
+    medico_id INT,
+    data_hora DATETIME NOT NULL,
+    tipo_consulta VARCHAR(50) NOT NULL,
+    FOREIGN KEY (paciente_id) REFERENCES pacientes(id),
+    FOREIGN KEY (medico_id) REFERENCES medicos(id)
+);
